04f2ccd65d6895e085b05b28f4fa1eba86e6133e
Mehmet Celik <mehmet@celik.be>
2025-02-09 20:46:13 +0100

    webdav

--- lib/data_collector/ext/x-open-uri-and-write.rb
+++ lib/data_collector/ext/x-open-uri-and-write.rb
@@ -9,7 +9,35 @@ require 'open-uri-and-write/usernames'
 require 'open-uri-and-write/credentials_store'
 require_relative 'file'
 require 'open-uri-and-write/dir_extensions'
-require 'open-uri-and-write/kernel_extensions'
+#require 'open-uri-and-write/kernel_extensions'
+
+# Kernel extensions
+# Careful monkeypatching
+module Kernel
+  private
+  alias open_uri_and_write_original open # :nodoc:
+
+  def open(name, *rest, &block) # :doc:
+    if name.respond_to?(:open)
+      name.open(*rest, &block)
+    elsif name.respond_to?(:to_s) and
+      name.to_s[/^(https?):\/\//] and
+      rest.size > 0 and
+      rest.first.to_s[/^[rwa]/]
+      webdav_agent = OpenUriAndWrite::Handle.new(name, rest)
+      if(block)
+        yield webdav_agent
+      else
+        return webdav_agent
+      end
+    else
+      mode, options = rest
+      open_uri_and_write_original(name.to_s, mode, **options, &block)
+    end
+  end
+
+  module_function :open
+end
 
 module OpenUriAndWrite
   class Handle < StringIO
--- lib/data_collector/version.rb
+++ lib/data_collector/version.rb
@@ -1,4 +1,4 @@
 # encoding: utf-8
 module DataCollector
-  VERSION = "0.55.0"
+  VERSION = "0.56.0"
 end
